dc5cdab2fe8659903e9e4274e7ecd7e8d7c7c29d
Hajra Faraz <farazhajra002@gmail.com>
2024-01-08 18:22:08 +0500

    initial diff commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Board from "./components/Administration/Board";
+
+const App = () => {
+  return (
+    <>
+      <Board />
+    </>
+  );
+};
+
+export default App;
+
+// import React from "react";
+// import Spark from "./Spark/Main File/Spark";
+
+// const App = () => {
+//   return (
+//     <>
+//       <Spark />
+//     </>
+//   );
+// };
+
+// export default App;
